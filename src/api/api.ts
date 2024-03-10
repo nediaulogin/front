999c045e074dee54a014937a31bcebaac8224b29
Elvis <nediaulog.gmail.com>
2024-03-09 21:17:52 -0300

    preparando chamadas api com axios para comecar a ultilizar o react query

---
+++ src/api/api.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+const URL = "https://65ecf85c0ddee626c9b10ef5.mockapi.io/projects"
+
+async function getProjects() {
+    const response = await axios.get(URL);
+    return response.data;
+}
+
+async function getProject(id: string) {
+    const response = await axios.get(`${URL}/${id}`);
+    return response.data;
+}
+
+async function createProject(project: any) {
+    const response = await axios.post(URL, project);
+    return response.data;
+}
+
+async function deleteProject(id: string) {
+    const response = await axios.delete(`${URL}/${id}`);
+    return response.data;
+}
+
+async function updateProject(project: any) {
+    const response = await axios.put(`${URL}/${project.id}`, project);
+    return response.data;
+}
+
+export const api = {
+    createProject,
+    deleteProject, getProject, getProjects, updateProject
+};
+
